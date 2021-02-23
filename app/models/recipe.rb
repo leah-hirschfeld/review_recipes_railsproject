c7a5286fb78c596754477edfdd3fee6be177cdc2
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-22 18:02:29 -0800

    validations for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
     has_many :reviews
+
+    validates :title, presence: true, uniqueness: true
+    validates :ingredients, presence: true
+    validates :instructions, presence: true
 end
