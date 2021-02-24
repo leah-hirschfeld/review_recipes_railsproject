9ff742d54b327ebb5edd11473c1b3fd005637e6c
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-23 20:27:16 -0800

    omniauth sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
     def omniauth
+        user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(16)
+        end
+
+        if user.valid?
+            redirect_to new_recipe_path
+        else
+            flash[:message] = user.errors.full_messages.join("")
+            redirect_to recipes_path
+        end
+    end
+
+    private
+
+    def auth
+        request.env('omniauth.auth')
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :recipes, through: :review
 
+    has_secure_password
+
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
 end
