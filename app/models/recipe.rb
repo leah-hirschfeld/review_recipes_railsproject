cd7b7d1e84fe9337af6879739d54c7f22f09ba25
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-23 19:24:07 -0800

    user model added and relationships between models fleshed out

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
     has_many :reviews
+    has_many :users, through: :review
 
     validates :title, presence: true, uniqueness: true
     validates :ingredients, presence: true
