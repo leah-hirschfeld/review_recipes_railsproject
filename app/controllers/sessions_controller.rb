1e3134b4e26a5432b329e392d6eba74fa6137233
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-23 20:38:19 -0800

    DRY logic from sessions controller to user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,6 @@
 class SessionsController < ApplicationController
     def omniauth
-        user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
-            u.username = auth['info']['name']
-            u.email = auth['info']['email']
-            u.password = SecureRandom.hex(16)
-        end
+        user = User.create_from_omniauth(auth)
 
         if user.valid?
             session[:user_id] = user.id
