0607ccd726ab061306d9230ce9aa9253c280e158
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-23 19:25:10 -0800

    validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :recipes, through: :review
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
