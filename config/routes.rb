e5d778f0dc4649e32e36f57c4642a5cec6be67a0
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-23 20:05:12 -0800

    omniauth action in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :recipes
+
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
