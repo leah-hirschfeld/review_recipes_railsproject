938920cf46715555477b59ebfa7ee7c1cb91a7d3
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-25 14:32:17 -0800

    new view for reviews added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+
+    def new
+        @review = Review.new
+    end
+
+    def index
+    end
+
+    def create
+    end
+    
+    def edit
+    end
 end
