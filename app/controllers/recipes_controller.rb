b52d37e30109d6643e2a2255f179b29712e307ab
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-24 21:30:52 -0800

    associate current user to recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,7 +14,8 @@ class RecipesController < ApplicationController
     end
     
     def create
-        @recipe = Recipe.new
+        @recipe = Recipe.new(recipe_params)
+        @recipe = current_user.recipes.build(recipe_params)
         if @recipe.valid?
             @recipe.save
             redirect_to recipe_path(@recipe)
