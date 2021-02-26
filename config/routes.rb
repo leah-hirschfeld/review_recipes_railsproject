97084ce614cb277e5e790b01d14dfb8e737c67d3
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-02-26 13:56:43 -0800

    nested routes for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :recipes
+  resources :recipes, only: [:new, :create, :show, :index] do 
+    resources :reviews
+  end
+
 
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
